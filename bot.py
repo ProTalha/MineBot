6d8dd26934abcebe68612addf05b31981d1fa55d
ProTalha <47188372+ProTalha@users.noreply.github.com>
2019-01-31 08:59:32 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,6 +16,31 @@ async def sword():
 
 @bot.command()
 async def nub():
-	await bot.say('u are nub -_-''')	
+	await bot.say('u are nub -_-')
+
+@bot.command()
+async def list():
+	await bot.say('All -Commands-')
+	await bot.say('/sword')
+	await bot.say('Kills You')
+	await bot.say('/nub')
+	await bot.say('Says Noob')
+	await bot.say('/creeper')
+	await bot.say('Sends Link Of A Creeper Pitcure')
+	await bot.say('/diaset')
+	await bot.say('Gives You Diamond Armor')
+	await bot.say('/creator')
+	await bot.say('Tells More About Creator')
+	
+@bot.command()
+async def creeper():	await bot.say('https://goo.gl/images/phsPwJ')
+
+@bot.command()
+async def diaset():
+	await bot.say('You Weared Diamond Armor!')
+	
+@bot.command()
+async def creator():
+	await bot.say('My Creators Name Is ProTalha You Can Join His Server From Here https://discord.gg/uR8VJbg')
 			
 bot.run('NTQwMTkzMjMxNTYwOTAwNjA4.DzNXNg.T_kBaIOzokaKinBIs_MCkowOxzc')
